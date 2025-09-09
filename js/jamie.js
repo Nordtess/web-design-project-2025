32341a5c6a74f8bef86a91952db0ca1e6a9514a9
Nordtess <nordtessj@gmail.com>
2025-09-09 23:52:59 +0200

    Mac2 (#8)
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie
    
    * updating jamie

---
+++ js/jamie.js
@@ -0,0 +1,5 @@
+//alert("det funkar")
+
+//document.getElementById("random-js-test").innerHTML = "<p>hello but me a dog!</p>";
+
+//document.getElementById("random-js-test").innerText = "Hello world!";
