9d6a627784be5b2a210d937d6e6951a6b66e40ce
Noccoxp <jamielarssonb@gmail.com>
2025-09-11 21:34:08 +0200

    working on jamie.html

--- js/jamie.js
+++ js/jamie.js
@@ -6,3 +6,40 @@
 
 
 
+
+
+
+
+
+
+
+function correctCalculation(answer) {
+    return parseInt(answer) === 2;
+}
+
+
+
+function showIlluminati(answer) {
+    if (correctCalculation(answer))
+        if (!document.getElementById("illuminati")) {
+            alert("Unbelivable, you are a genius!");
+            const img = document.createElement("img");
+            img.id = "illuminati";
+            img.src = "/images/abdimalik.png";
+            img.alt = "Illuminati";
+            img.style.opacity = "0";
+            img.style.transition = "opacity 10s";
+            document.body.appendChild(img);
+            setTimeout(() => { img.style.opacity = "1"; }, 10); // Fade in
+        }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.forms["calculator"];
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const answer = form["calculatorInput"].value;
+        showIlluminati(answer);
+    });
+});
